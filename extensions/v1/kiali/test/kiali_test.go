d83576dbda2d2031bcb56f2dcd159149c23f9691
Rick Ducott <rick.ducott@solo.io>
2019-05-14 11:14:56 -0400

    Revert "Add manifesttestutils"
    This reverts commit dc7b79bae9aa90d453687c7ee09d0265485a42e1.

--- extensions/v1/kiali/test/kiali_test.go
+++ extensions/v1/kiali/test/kiali_test.go
@@ -2,30 +2,20 @@ package test
 
 import (
 	"context"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/solo-io/go-utils/manifesttestutils"
-	"github.com/solo-io/service-mesh-hub/api/v1"
+	"github.com/solo-io/go-utils/installutils/kuberesource"
+	v1 "github.com/solo-io/service-mesh-hub/api/v1"
 	"github.com/solo-io/service-mesh-hub/pkg/render"
 	"github.com/solo-io/service-mesh-hub/test"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 )
 
 var _ = Describe("kiali", func() {
-
-	const (
-		namespace = "istio-system"
-		name      = "kiali"
-		meshName  = "istio"
-
-	)
-
 	var (
 		spec       *v1.ApplicationSpec
 		versionMap map[string]*v1.VersionedApplicationSpec
-		labels     = map[string]string {
-			"app": "kiali",
-		}
 	)
 
 	BeforeEach(func() {
@@ -36,45 +26,32 @@ var _ = Describe("kiali", func() {
 		}
 	})
 
-	Context("0.12 with default values", func() {
+	Context("0.12", func() {
 		var (
-			version      *v1.VersionedApplicationSpec
-			inputs       render.ValuesInputs
-			testManifest TestManifest
+			version   *v1.VersionedApplicationSpec
+			inputs    render.ValuesInputs
+			resources kuberesource.UnstructuredResources
 		)
 
 		BeforeEach(func() {
 			version = versionMap["0.12"]
 			inputs = render.ValuesInputs{
-				Name:             name,
-				FlavorName:       meshName,
-				InstallNamespace: namespace,
+				Name:             "kiali",
+				FlavorName:       "istio",
+				InstallNamespace: "istio-system",
 				MeshRef: core.ResourceRef{
-					Name:      meshName,
-					Namespace: namespace,
+					Name:      "istio",
+					Namespace: "istio-system",
 				},
 				SpecDefinedValues: version.ValuesYaml,
 			}
 			rendered, err := render.ComputeResourcesForApplication(context.TODO(), inputs, version)
 			Expect(err).NotTo(HaveOccurred())
-			testManifest = NewTestManifestWithResources(rendered)
+			resources = rendered
 		})
 
 		It("has the correct number of resources", func() {
-			Expect(testManifest.NumResources()).To(Equal(7))
-		})
-
-		It("has a demo secret", func() {
-			rb := ResourceBuilder{
-				Name: name,
-				Namespace: namespace,
-				Data: map[string]string {
-					"username": "admin",
-					"passphrase": "admin",
-				},
-				Labels: labels,
-			}
-			testManifest.ExpectSecret(rb.GetSecret())
+			Expect(len(resources)).To(Equal(7))
 		})
 	})
 })
